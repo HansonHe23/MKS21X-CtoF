9f8ae1aad672b62e9d7401caf17e3026293c2e3a
HansonHe23 <vinisher12@gmail.com>
2021-09-23 23:45:19 -0400

    Fixed semicolon

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,6 +1,6 @@
 public class CtoFTester {
   public static void main(String[] args){
-    System.out.println(fahrenheitToCelsius( 9.0 ) )
+    System.out.println(fahrenheitToCelsius( 9.0 ) );
   }
   public static double celsiusToFahrenheit(double celsius) {
     return (celsius * 9.0 / 5.0) +32.0;
