d2815c32cc59fbbee42d47e593e4aa9ac95582ba
HansonHe23 <vinisher12@gmail.com>
2021-09-23 23:41:19 -0400

    first code

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,9 @@
-public class CtoFTester 
+public class CtoFTester {
+  public static void main(String[] args){}
+  public static double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9.0 / 5.0) +32.0;
+  }
+  public static double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32.0) * 5.0 / 9.0;
+  }
+}
